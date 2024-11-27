3f267675ee8b9f2a4b3480b4cd669ff56c6e7078
bunnybumbum <bunnyyellow1209@gmail.com>
2024-11-27 10:53:56 +0530

    feat : added joiSchema to user authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,50 +1,58 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/usersSchema.js'
-import bcrypt from 'bcrypt'
-import CustomError from '../utils/customError.js'
+import jwt from "jsonwebtoken";
+import User from "../models/usersSchema.js";
+import bcrypt from "bcrypt";
+import CustomError from "../utils/customError.js";
+import { joiUserSchema } from "../models/joiValSchema.js";
 
 // createToken
-const createToken = (id)=>{
-    return jwt.sign({id}, process.env.JWT_TOKEN);
-}
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_TOKEN);
+};
 
-const userRegister = async (req,res)=>{
-        const {name,email,password} = req.body
-        // emailChecking
-        const exists = await User.findOne({email})
-        if(exists){
-           return res.status(400).send("user exists")
-        }
-        // password Hashing + salt
-        const hashedPassword = await bcrypt.hash(password,10) 
-        const newUser = new User({
-            name,
-            email,
-            password:hashedPassword
-        })
-        
-        // adding user to db
-        await newUser.save()
-        res.json({success:true , message:"user registered"})
-}
+const userRegister = async (req, res,next) => {
+  //validating with joi
+  const { value, error } = joiUserSchema.validate(req.body);
+  const { name, email, password } = value;
+  if(error){
+    return next(new CustomError(error.details[0].message,400))
+  }
+  // emailChecking
+  const exists = await User.findOne({ email });
+  if (exists) {
+    return next(new CustomError("User already exists",400))
+  }
+  // password Hashing + salt
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
+  // adding user to db
+  await newUser.save();
+  res.json({ success: true, message: "user registered" });
+};
 
-const loginUser = async(req,res,next)=>{
-        const {email , password} = req.body;
-        const user = await User.findOne({email})
-        
-        if(!user){
-            return next(new CustomError("User doesn't exist",401))
-        }
+const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-        const isMatch = await bcrypt.compare(password,user.password)
-        if(!isMatch){
-            return next(new CustomError("Incorrect password",401))
-        }
-        // creating token for logged user
-        const token = createToken(user._id)
-        res.json({success:true,token})
-}
+  if (!user) {
+    return next(new CustomError("User doesn't exist", 401));
+  }
 
+  if(user.isBlocked){
+    return next(new CustomError("User is Blocked", 403));
+  }
 
-export {userRegister,loginUser}
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return next(new CustomError("Incorrect password", 401));
+  }
+  // creating token for logged user
+  const token = createToken(user._id);
+  res.json({ message:"You've successfully logged in", token });
+};
+
+export { userRegister, loginUser };
---
+++ server/models/joiValSchema.js
@@ -0,0 +1,23 @@
+import joi from 'joi'
+
+const joiUserSchema = joi.object({
+    name:joi.string().required(),
+    email:joi.string().email().required(),
+    password:joi.string().min(4).required(),
+    isBlocked:joi.boolean().default(false).optional(),
+    refreshToken:joi.string().optional()
+})
+
+const joiProductSchema = joi.object({
+        name:joi.string().required(),
+        type:joi.string().required(),
+        image:joi.string().required(),
+        price:joi.number().required(),
+        qty:joi.number().required(),
+        description:joi.string().optional(),
+        brand:joi.string().required(),
+        rating:joi.number().required().min(1).max(5),
+        reviews:joi.number().optional()
+})
+
+export {joiUserSchema,joiProductSchema}
